5c8559bce3b7def10b89a3f8794ca319b50e4550
ForestOnTheLand <2200013136@stu.pku.edu.cn>
2023-12-25 13:51:02 +0800

    newest version

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def mutator(melody: Melody) -> None:
 
 def evaluator(x: Melody) -> float:
     return (0.8 * F.interval_score(x) + 0.4 * F.rhythm_score(x) +
-            0.6 * F.tonality_score(x, 'minor') + 0.2 * F.stable_score(x) +
+            0.6 * F.tonality_score(x, 'major') + 0.2 * F.stable_score(x) +
             0.6 * F.boundary_score(x) - F.density_penalty(x) - F.stop_penalty(x) -
             F.rest_penalty(x) - F.consecutive_penalty(x, 7) - F.range_penalty(x, 18) -
             F.variety_penalty(x, 5) - F.lonely_penalty(x))
@@ -34,29 +34,18 @@ if __name__ == '__main__':
 
     algorithm = GeneticAlgorithm(
         population=[generator() for _ in range(10)],  # Initial population
+        # population=[melodies.d_major_canon for _ in range(10)],
         mutation_rate=0.2,
-        epoch=1000,
+        epoch=500,
         score_function=evaluator,
         mutate_function=mutator,
         cross_function=lambda x, y: op.two_points_cross(x, y, random_interval(32)),
         early_stop=False,
-        record=True,
+        length=32,
+        # record=True,
     )
 
     algorithm.evolve()
-
-    for i in [10, 50, 100, 200, 500, 1000]:
-        print(evaluator(algorithm.music[i]))
-
-    # x, y = [], []
-    # for i, music in enumerate(algorithm.music):
-    #     x.append(i)
-    #     y.append(evaluator(music))
-    # plt.plot(x, y)
-    # plt.xlabel('epoch')
-    # plt.ylabel('score')
-    # plt.show()
-    # exit(0)
 
     melody = algorithm.choose_best()
     print(melody)
@@ -64,10 +53,10 @@ if __name__ == '__main__':
     print(
         "Scores interval:{:.2f}, tonality:{}, rhythm:{:.2f}, stable:{:.2f}, boundary:{:.2f}".format(
             F.interval_score(melody),
-            F.get_tonality(melody, 'C major'),
+            F.get_tonality(melody, 'major'),
             F.rhythm_score(melody),
             F.stable_score(melody),
             F.boundary_score(melody),
         ))
     save_midi(melody, './tmp.mid')
-    # play_midi('./tmp.mid')
+    play_midi('./tmp.mid')
